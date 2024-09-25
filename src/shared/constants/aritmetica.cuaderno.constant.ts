8d40cc4626881dc2706256f02ff8c51307fec950
auriel1 <auriel@smartsoftlabs.com>
2024-09-25 01:33:47 -0500

    Se refactoriza calidad de imagenes

--- src/shared/constants/aritmetica.cuaderno.constant.ts
+++ src/shared/constants/aritmetica.cuaderno.constant.ts
@@ -3,187 +3,187 @@ import { CuadernoModel } from "../models/cuaderno.model";
 export const C_ARITMETICA: CuadernoModel[] = [
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 1',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0001.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 2',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0002.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 3',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0003.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 4',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0004.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 5',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0005.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 6',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0006.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 7',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0007.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 8',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0008.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 9',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 0009.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 10',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00010.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 11',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00011.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 12',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00012.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 13',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00013.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 14',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00014.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 15',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00015.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 16',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00016.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 17',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00017.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 18',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00018.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 19',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00019.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 20',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00020.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 21',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00021.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 22',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00022.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 23',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00023.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 24',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00024.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 25',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00025.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 26',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00026.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 27',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00027.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 28',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00028.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 29',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00029.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 30',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00030.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
+        descripcionPagina: 'Página 31',
         fecha:'Septiembre 24 de 2024',
         ruta:'aritmetica/aritmetica - 00031.jpg'
     },
