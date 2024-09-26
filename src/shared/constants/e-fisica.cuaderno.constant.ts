2146427b972a0d1b8a95f78da10d81ee09497a64
auriel1 <auriel@smartsoftlabs.com>
2024-09-26 08:50:42 -0500

    cuaderno de educacion fisica

--- src/shared/constants/e-fisica.cuaderno.constant.ts
+++ src/shared/constants/e-fisica.cuaderno.constant.ts
@@ -3,20 +3,93 @@ import { CuadernoModel } from "../models/cuaderno.model";
 export const C_E_FISICA: CuadernoModel[] = [
     {
         alt: '',
-        descripcionPagina: '',
-        fecha:'Septiembre 18 de 2024',
-        ruta:'eFisica/eFisica-0001.jpg'
+        descripcionPagina: 'Página 1',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0001.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
-        fecha:'Septiembre 18 de 2024',
-        ruta:'eFisica/eFisica-0002.jpg'
+        descripcionPagina: 'Página 2',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0002.jpg'
     },
     {
         alt: '',
-        descripcionPagina: '',
-        fecha:'Septiembre 18 de 2024',
-        ruta:'eFisica/eFisica-0003.jpg'
+        descripcionPagina: 'Página 3',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0003.jpg'
     },
+    {
+        alt: '',
+        descripcionPagina: 'Página 4',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0004.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 5',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0005.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 6',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0006.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 7',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0007.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 8',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0008.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 9',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 0009.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 10',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00010.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 11',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00011.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 12',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00012.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 13',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00013.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 14',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00014.jpg'
+    },
+    {
+        alt: '',
+        descripcionPagina: 'Página 15',
+        fecha:'Septiembre 26 de 2024',
+        ruta:'eFisica/educacion-fisica - 00015.jpg'
+    },
+
 ]
